6589aa8bbed85e0bd8d04e1c1c734e29aae2e359
NULL <lixiang3.1416@qq.com>
2018-10-19 22:46:25 +0800

    receive correctly, cannot send

---
+++ ir_test.ino/ir_test.ino.ino
@@ -0,0 +1,43 @@
+/*
+ * IRremote: IRsendDemo - demonstrates sending IR codes with IRsend
+ * An IR LED must be connected to Arduino PWM pin 3.
+ * Version 0.1 July, 2009
+ * Copyright 2009 Ken Shirriff
+ * http://arcfn.com
+ */
+
+
+#include <IRremote.h>
+
+IRsend irsend;
+
+int RECV_PIN = 7;
+
+IRrecv irrecv(RECV_PIN);
+
+decode_results results;
+
+void setup()
+{
+  Serial.begin(9600);
+  // In case the interrupt driver crashes on setup, give a clue
+  // to the user what's going on.
+  Serial.println("Enabling IRin");
+  irrecv.enableIRIn(); // Start the receiver
+  Serial.println("Enabled IRin");
+}
+
+void loop() {
+  for (int i = 0; i < 3; i++) {
+    irsend.sendSony(0xa90, 4);
+    delay(40);
+  }
+  delay(1);
+  Serial.println("send");
+  if (irrecv.decode(&results)) {
+    Serial.println(results.value, HEX);
+    irrecv.resume(); // Receive the next value
+  }
+  delay(100);
+  delay(5000); //5 second delay between each signal burst
+}
